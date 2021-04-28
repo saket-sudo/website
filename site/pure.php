232fd9fa118e3272c0f7e018a543aa9f33ea2425
saket-sudo <77758954+saket-sudo@users.noreply.github.com>
2021-04-28 11:13:58 -0500

    Add files via upload

---
+++ site/pure.php
@@ -0,0 +1,30 @@
+<?php
+  $name = $_POST['name'];
+  $visitor_email = $_POST['email'];
+  $message = $_POST['message'];
+?>
+
+<?php
+	$email_from = "$visitor_email";
+
+	$email_subject = "New Form submission";
+
+	$email_body = "You have received a new message from the user $name.\n
+                            Here is the message:\n $message";
+?>
+
+
+<?php
+
+  $to = "flakeartiste@gmail.com";
+
+  $headers = "From: $email_from \r\n";
+
+  $headers .= "Reply-To: $visitor_email \r\n";
+
+  mail($to,$email_subject,$email_body,$headers);
+
+ ?>
+
+
+
