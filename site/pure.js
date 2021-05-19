516083ef629a6fda7043566526bda9b7811acb2a
saket-sudo <77758954+saket-sudo@users.noreply.github.com>
2021-05-19 10:21:02 -0500

    Update pure.js

--- site/pure.js
+++ site/pure.js
@@ -15,7 +15,7 @@ $(window).scroll(function() {
    }
 });
 
-document.writeln('<script src="http://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js" type="text/javascript"></script>');
+document.writeln('<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js" type="text/javascript"></script>');
 
 
 
